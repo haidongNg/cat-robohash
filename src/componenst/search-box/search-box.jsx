e87c5439f6004ef4f23c5f034c4eceb4d5ca4b8a
Dong Nguyen <haidongNg@linux>
2021-04-18 20:09:39 +0700

    revert code deploy

---
+++ src/componenst/search-box/search-box.jsx
@@ -0,0 +1,13 @@
+import './search-box.style.scss';
+const SearchBox = ({placeholder, handleChange}) => {
+  return (
+    <input
+      className="search"
+      type="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+    />
+  );
+};
+
+export default SearchBox;
